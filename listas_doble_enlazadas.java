d0532d99c2b74d4640fc4cb9c880befcfdac71d1
Miguel Angel <youngpl3asant@gmail.com>
2025-03-13 21:12:13 -0600

    ultimo cambio_hoy

--- listas_doble_enlazadas.java
+++ listas_doble_enlazadas.java
@@ -5,6 +5,7 @@ public class listas_doble_enlazadas {
         Scanner vard = new Scanner(System.in);
         ArrayList<Integer> list = new ArrayList<>();
         
+        
         // insertar en la lista:
         list.add(15);
         list.add(16);
@@ -12,14 +13,14 @@ public class listas_doble_enlazadas {
         list.add(18);
         list.add(19);
         list.add(20);
-
+ /* 
         //insertar al inicio de la lista
         list.addFirst(14);        
         
         // insertar al final de la lista
         list.addLast(21);
 
-        // insertar antes de un numero en una lista de array list      
+        // insertar antes de un numero en una lista      
         System.out.println("que numero desea agregar: ");
         int var = vard.nextInt();
         System.out.println("antes de que numero desea agregarlo: ");
@@ -29,16 +30,55 @@ public class listas_doble_enlazadas {
         
         
         for (int i = 0; i < list.size(); i++) {
-        if (list.get(i) == var1) {     
+        if (list.get(i) == var1) {   
             list.add(i,var);
+            break;
+            }
             //agrego la posicion
-        }else{
+        }
+ 
+        // insertar despues de un numero en una lista     
+        System.out.println("que numero desea agregar: ");
+        int var2 = vard.nextInt();
+        System.out.println("despues de que numero desea agregarlo: ");
+        int va3 = vard.nextInt();
 
-        }
-        }
+
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) == var2) {   
+                list.add(i+1,va3);
+                break;
+                }
+                //agrego la posicion
+            }
+
+        //borrar un elemento de la lista
         
+        System.out.println("que elemento desea borrar: ");    
+        int delete_try = vard.nextInt();
+        for (int i = 0; i < list.size(); i++) {
+            while(list.get(i) == delete_try){
+                list.remove(i);
+                break;
+            } 
+          
+        }
+        // operacion de recorrido de inicio a fin
+*/
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+
+        // operacion de recorrido de inicio a fin
+        System.out.println("de fin a inicio");
+
+        for (int i = list.size() - 1; i >= 0; i++) {
+            System.out.println(list.get(i));
+        }
+
+
 
         // mostrar todos los elementos
-        System.out.println(list);
+        //System.out.println(list);
     }
 }
